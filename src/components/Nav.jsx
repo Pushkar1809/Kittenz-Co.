8a778dcdf512e8f50815b98fe2844153ebfbb2b4
PUSHKAR BORKAR <itsabappithing@PUSHKARs-Air.bbrouter>
2020-11-15 13:25:27 +0530

    Added Nav Bar Component and removed bolier plate.

---
+++ src/components/Nav.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Nav extends Component {
+  state = {
+    links: [
+      { id: 1, value: "Home" },
+      { id: 2, value: "Kitties" },
+      { id: 3, value: "Contact" },
+      { id: 4, value: "About" },
+    ],
+  };
+  render() {
+    return (
+      <nav>
+        <div className="nav__logo">Kittenz Co.</div>
+        <ul className="nav__links">
+          {this.state.links.map((link) => (
+            <li key={this.state.links.id}>
+              <Link to="/{link.value}">{link.value}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
